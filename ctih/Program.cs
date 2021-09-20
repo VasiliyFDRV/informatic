e9f8cf2593a2876b983c54478256898f8177e197
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-09-20 15:24:02 +0500

    task1

---
+++ ctih/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ctih
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Never gonna give you up");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
