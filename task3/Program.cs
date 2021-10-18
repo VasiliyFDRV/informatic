4e2f7b530376f4f56b4bd8db8a4f66e90fe97fad
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-10-18 13:55:33 +0500

    Task 3 done!

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, x, e;
+            Console.WriteLine("enter x:");
+            x = double.Parse(Console.ReadLine());
+            e = Math.E;
+            y = Math.Pow(e,(1-(x*x))/(1+(x*x)));
+            Console.WriteLine("y = ", y);
+        }
+    }
+}
