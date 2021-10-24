1ede39e5932ecea5f4ba37ed62902c95de95185f
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-10-25 01:13:17 +0500

    task 3 redoned!

---
+++ task3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("enter x:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("y = " + Funk(x));
+        }
+        static double Funk(double x)
+        {
+            return Math.Pow(Math.E, (1 - (x * x)) / (1 + (x * x)));
+        }
+    }
+}
