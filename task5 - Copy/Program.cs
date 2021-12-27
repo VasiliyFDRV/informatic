fb818f8a86933d6960a299d2cda5fab00281a90c
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-12-28 01:30:56 +0500

    5.1

---
+++ task5 - Copy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+        static string MID(string a)
+        { 
+            a = a.Replace("А", "A");
+            a = a.Replace("Б", "B");
+            a = a.Replace("В", "V");
+            a = a.Replace("Г", "G");
+            a = a.Replace("Д", "D");
+            a = a.Replace("Е", "E");
+            a = a.Replace("Ё", "E");
+            a = a.Replace("Ж", "ZH");
+            a = a.Replace("З", "Z");
+            a = a.Replace("И", "I");
+            a = a.Replace("К", "K");
+            a = a.Replace("Л", "L");
+            a = a.Replace("М", "M");
+            a = a.Replace("Н", "N");
+            a = a.Replace("О", "O");
+            a = a.Replace("П", "P");
+            a = a.Replace("Р", "R");
+            a = a.Replace("С", "S");
+            a = a.Replace("Т", "T");
+            a = a.Replace("У", "U");
+            a = a.Replace("Ф", "F");
+            a = a.Replace("Х", "KH");
+            a = a.Replace("Ц", "TS");
+            a = a.Replace("Ч", "CH");
+            a = a.Replace("Ш", "SH");
+            a = a.Replace("Щ", "SHCH");
+            a = a.Replace("Ъ", "IE");
+            a = a.Replace("Ы", "Y");
+            a = a.Replace("Ь", "");
+            a = a.Replace("Э", "E");
+            a = a.Replace("Ю", "IU");
+            a = a.Replace("Я", "IA");
+            return a;
+        }
+    }
+}
