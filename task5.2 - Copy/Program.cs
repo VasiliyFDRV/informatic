4656fb861e3554d3772c35cfc81f453ceb6e719a
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-12-28 01:38:48 +0500

    commit

---
+++ task5.2 - Copy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "indul'gencia";
+            string result1 = str.Substring(6,1)
+                + str.Substring(3,3)
+                + str.Substring(2,1)
+                + str.Substring(7,2);
+            Console.WriteLine(result1); //выводит "gul'den"
+
+            string result2 = str.Substring(9, 1)
+                + str.Substring(7, 1)
+                + str.Substring(4, 2);
+            Console.WriteLine(result2); //выводит "cel'"
+
+        }
+    }
+}
