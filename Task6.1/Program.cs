ea00830e57eca9d2530b1d4bc5151f03d15e5756
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-12-29 01:21:57 +0500

    6 done

---
+++ Task6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Check(k,m,n));
+        }
+
+        static bool Check(int k, int m, int n)
+        {
+            return (k<0 && m<0 && n<0 && (k%2!=0 || m % 2 != 0 || n % 2 != 0));
+        }
+    }
+}
---
+++ Task6.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            var y = double.Parse(Console.ReadLine());
+
+            static bool Check(double x, double y)
+            {
+                return (y > -2 && y < 1.5);
+            }
+        }
+    }
+}
