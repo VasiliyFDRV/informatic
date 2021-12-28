d24973818bbb5dc720b15be0fe8f633e15a2db1f
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-12-29 01:45:14 +0500

    8 partly done

---
+++ Task8.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter quantity of digets");
+            var k = int.Parse(Console.ReadLine());
+            int result = 1;
+            int c;
+
+            for (int i = 1; i <= k; i++)
+            {
+                c = n % 10;
+                n = n / 10;
+                result *= c;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ task8.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 0;
+            Console.WriteLine("Enter quantity of pairs");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i<=n; i++)
+            {
+                Console.WriteLine("Enter R for both resistors");
+                r+= (double.Parse(Console.ReadLine()) + double.Parse(Console.ReadLine()))/2; ;
+            }
+            Console.WriteLine(r);
+        }
+    }
+}
