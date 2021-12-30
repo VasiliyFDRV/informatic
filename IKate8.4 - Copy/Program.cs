a05b749678b3feb5f05e11417caefb27ae4a2985
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2021-12-30 11:32:59 +0500

    cummit

---
+++ IKate8.4 - Copy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IKate8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int n = int.Parse(Console.ReadLine());
+            int a;
+            int aMin = 10;
+
+            while (n>0)
+            {
+                a = n % 10;
+                n = n / 10;
+                if (a<aMin) { aMin = a;}
+            }
+
+            Console.WriteLine(aMin);
+        }
+    }
+}
