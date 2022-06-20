cccc0b0c894ef94b5dc50119944d1067c2ce0ac9
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2022-06-20 16:28:02 +0500

    2048 done!

---
+++ 2048(3)/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+
+        Model model = new Model();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.A)
+            {
+                model.Left();
+                Rewrite();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            model.Start();
+            Rewrite();
+        }
+
+        private void Rewrite()
+        {
+            button1.Text = $"{model.map1[0, 0]}";
+            button2.Text = $"{model.map1[0, 1]}";
+            button3.Text = $"{model.map1[0, 2]}";
+            button4.Text = $"{model.map1[0, 3]}";
+            button5.Text = $"{model.map1[1, 0]}";
+            button6.Text = $"{model.map1[1, 1]}";
+            button7.Text = $"{model.map1[1, 2]}";
+            button8.Text = $"{model.map1[1, 3]}";
+            button9.Text = $"{model.map1[2, 0]}";
+            button10.Text = $"{model.map1[2, 1]}";
+            button11.Text = $"{model.map1[2, 2]}";
+            button12.Text = $"{model.map1[2, 3]}";
+            button13.Text = $"{model.map1[3, 0]}";
+            button14.Text = $"{model.map1[3, 1]}";
+            button15.Text = $"{model.map1[3, 2]}";
+            button16.Text = $"{model.map1[3, 3]}";
+        }
+    }
+}
---
+++ 2048(3)/Model.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    class Model
+    {
+        bool isGameover;
+        Map map;
+        public int[,] map1 = new int[4, 4];
+        static Random rnd = new Random();
+
+        public Model()
+        {
+            map = new Map();
+        }
+
+        public void Start()
+        {
+            for (int x = 0; x < 4; x++)
+                for (int y = 0; y < 4; y++) 
+                { map1[x,y] = 0; }
+            AddRandom(map1);
+            AddRandom(map1);
+        }
+
+        private void AddRandom(int[,] map)
+        {
+            if (isGameover) return;
+            for (int j = 0; j < 100; j++)
+            {
+                int x = rnd.Next(0, 4);
+                int y = rnd.Next(0, 4);
+                if (map[x,y] == 0)
+                {
+                    int n = rnd.Next(1, 10);
+                    if (n != 4) n = 2;
+                    map[x,y] = n;
+                    return;
+                }
+            }
+        }
+
+
+        //private void AddRandom()
+        //{
+        //    if (isGameover) return;
+        //    for (int j = 0; j < 100; j ++)
+        //    {
+        //        int x = rnd.Next(0, 4);
+        //        int y = rnd.Next(0, 4);
+        //        if (map.Get(x, y) == 0)
+        //        {
+        //            int n = rnd.Next(1, 10);
+        //            if (n != 4) n = 2;
+        //            map.Set(x, y, n);
+        //            return;
+        //        }
+        //    }
+        //}
+
+
+
+        //private void Lift(int x, int y, int sx, int sy)
+        //{
+        //    if ( map.Get(x,y)!=0)
+        //    {
+        //        while
+        //    }
+        //}
+
+        private List<List<int>> MoveX(int[,] map)
+        {
+            var l1 = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                l1.Add(map[i, 1]);
+            }
+
+            var l2 = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                l2.Add(map[i, 1]);
+            }
+
+            var l3 = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                l3.Add(map[i, 1]);
+            }
+
+            var l4 = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                l4.Add(map[i, 1]);
+            }
+
+            var finalL = new List<List<int>>() { l1, l2, l3, l4 };
+            foreach (List<int> l in finalL)
+            {
+                foreach (int i in l)
+                    if (i == 0)
+                        l.Remove(i);
+            }
+            return finalL;
+        }
+
+        public void Left()
+        {
+            int[,] map2 = map1;
+            var l = MoveX(map1);
+            for (int j = 0; j < 4; j++)
+            { 
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i < l[j].Count) map2[i, j] = l[j][i];
+                    else map2[i, j] = 0;
+                }
+            }
+            AddRandom(map2);
+            map1 = map2;
+        }
+
+        public void Right()
+        { }
+
+        public void Up()
+        { }
+
+        public void Down()
+        { }
+    }
+}
