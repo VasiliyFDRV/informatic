7fb8b0d48b6241c1c8a98d2a96c90aceadd2cbca
VasiliyFDRV <vasilii.fdrv.2003@gmail.com>
2023-06-12 21:34:20 +0500

    Task5

---
+++ Task5 2023/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_2023
+{
+    public struct Record
+    {
+        public int ClientID;
+        public int  Year;   
+        public int  Month;
+        public int Duration;
+    }
+}
